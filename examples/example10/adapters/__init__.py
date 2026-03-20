b7587f5168760703a27b040934604746417c4b85
Francisco Salazar <franquant@gmail.com>
2026-03-20 14:21:46 -0500

    Remove migrated example10 from demos repo

--- examples/example10/adapters/__init__.py
+++
@@ -1,9 +0,0 @@
-from .market_adapter import MarketAdapter
-from .mcp_macro_adapter import MCPMacroAdapter
-from .mcp_web_adapter import MCPWebAdapter
-
-__all__ = [
-    "MarketAdapter",
-    "MCPWebAdapter",
-    "MCPMacroAdapter",
-]
